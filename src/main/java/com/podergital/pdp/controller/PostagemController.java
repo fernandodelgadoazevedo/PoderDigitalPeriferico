1e8c38eb1d8361f3175e2d6e712d5db5dbbac740
Vinicius Fernando <viniferan@gmail.com>
2023-02-02 10:22:53 -0300

    projeto 2.0

---
+++ src/main/java/com/podergital/pdp/controller/PostagemController.java
@@ -0,0 +1,78 @@
+package com.podergital.pdp.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.podergital.pdp.model.PostagemModel;
+import com.podergital.pdp.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class PostagemController {
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<PostagemModel>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<PostagemModel>getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<PostagemModel>>getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	@PostMapping
+	public ResponseEntity<PostagemModel> post (@Valid @RequestBody PostagemModel postagem){
+		if(temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+	@PutMapping
+	public ResponseEntity<PostagemModel> put (@Valid @RequestBody PostagemModel postagem){
+		if (postagemRepository.existsById(postagem.getId())) {
+			if(temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+			
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<PostagemModel> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		postagemRepository.deleteById(id);
+	}
+}
